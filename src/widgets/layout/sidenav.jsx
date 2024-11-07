1b94abf06726766b563f71bb967fa20066a5853f
Nimit Gupta <gnimit146@gmail.com>
2024-11-07 20:43:25 +0530

    second commit

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -96,7 +96,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
 
 Sidenav.defaultProps = {
   brandImg: "/img/logo-ct.png",
-  brandName: "SimpleEnergy Fleet Dashboard",
+  brandName: "EV Vehicle Fleet Managment Dashboard",
 };
 
 Sidenav.propTypes = {
